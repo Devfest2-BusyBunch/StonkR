e98d1098e847497b7fa1a160998e27641e9bf000
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-08-14 14:06:30 +0530

    Login route created

--- src/components/Header.js
+++ src/components/Header.js
@@ -69,9 +69,9 @@ const MenuLinks = ({ isOpen }) => {
 				pt={[4, 4, 0, 0]}>
 				<MenuItem to="/">Home</MenuItem>
 				<MenuItem to="/how">How It works </MenuItem>
-				<MenuItem to="/faetures">Features </MenuItem>
+				<MenuItem to="/features">Features </MenuItem>
 				<MenuItem to="/pricing">Pricing </MenuItem>
-				<MenuItem to="/signup" isLast>
+				<MenuItem to="/login" isLast>
 					<Button
 						size="sm"
 						rounded="md"
