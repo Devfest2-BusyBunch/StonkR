d78ca0fe0bdf81ac78d8f2745313fe490a88badf
Swarnab Garang <swarnabgarang@gmail.com>
2021-08-15 19:45:00 +0530

    Trending bug fix

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,37 +5,37 @@ import Trending from "components/Trending";
 import { supabase } from "supabaseClient";
 
 const Dashboard = () => {
-	const [userID, setUserID] = useState(null);
-	const [userName, setUserName] = useState(null);
+    const [userID, setUserID] = useState(null);
+    const [userName, setUserName] = useState("");
 
-	useEffect(() => {
-		const update = async () => {
-			setUserID(JSON.parse(localStorage.getItem("userID")));
+    useEffect(() => {
+        const update = async () => {
+            setUserID(JSON.parse(localStorage.getItem("userID")));
 
-			const { data: userData, error } = await supabase
-				.from("users")
-				.select("user_id, username")
-				.eq("user_id", userID);
-			setUserName(userData[0].username);
-		};
+            const { data: userData, error } = await supabase
+                .from("users")
+                .select("user_id, username")
+                .eq("user_id", userID);
+            setUserName(userData[0].username);
+        };
 
-		update();
-	}, [userID]);
+        update();
+    }, [userID]);
 
-	return (
-		<Box>
-			<Box>
-				<Text>Dashboard</Text>
-				{userName && <Heading>Welcome {userName}</Heading>}
-			</Box>
-			<Box className="container side-bar">
-				<Trending />
-			</Box>
-			<Box className="container chart_outer">
-				<Candle />
-			</Box>
-		</Box>
-	);
+    return (
+        <Box>
+            <Box>
+                <Text>Dashboard</Text>
+                {userName && <Heading>Welcome {userName}</Heading>}
+            </Box>
+            <Box className="container side-bar">
+                <Trending />
+            </Box>
+            <Box className="container chart_outer">
+                <Candle />
+            </Box>
+        </Box>
+    );
 };
 
 export default Dashboard;
