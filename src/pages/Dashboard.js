279fa0bf3936ae04ea3aa2fed419b37fdb9d6f86
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-08-15 19:02:06 +0530

    Username added to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,32 @@
+import { Box, Text, Heading } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 import Candle from "components/Chart";
 import Trending from "components/Trending";
-import { Box } from "@chakra-ui/react";
+import { supabase } from "supabaseClient";
 
 const Dashboard = () => {
+	const [userID, setUserID] = useState(null);
+	const [userName, setUserName] = useState(null);
+
+	useEffect(() => {
+		const update = async () => {
+			setUserID(JSON.parse(localStorage.getItem("userID")));
+
+			const { data: userData, error } = await supabase
+				.from("users")
+				.select("user_id, username")
+				.eq("user_id", userID);
+
+			setUserName(userData[0].username);
+		};
+
+		update();
+	}, []);
+
 	return (
 		<Box>
+			<Text>Dashboard</Text>
+			{userName && <Heading>Welcome {userName}</Heading>}
 			<Box className="container side-bar">
 				<Trending />
 			</Box>
