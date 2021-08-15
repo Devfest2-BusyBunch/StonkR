b24ed2b7b89ed8f08db5ec88aa3a3378b4c3d8d1
Swarnab Garang <swarnabgarang@gmail.com>
2021-08-15 15:53:02 +0530

    app ui changes

--- src/Logo.js
+++ src/Logo.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Image, keyframes, usePrefersReducedMotion } from "@chakra-ui/react";
-import logo from "logo.svg";
 
 const spin = keyframes`
   from { transform: rotate(0deg); }
   to { transform: rotate(360deg); }
 `;
 
-export const Logo = props => {
-	const prefersReducedMotion = usePrefersReducedMotion();
+export const Logo = (props) => {
+    const prefersReducedMotion = usePrefersReducedMotion();
 
-	const animation = prefersReducedMotion
-		? undefined
-		: `${spin} infinite 20s linear`;
+    const animation = prefersReducedMotion
+        ? undefined
+        : `${spin} infinite 20s linear`;
 
-	return <Image animation={animation} src={logo} {...props} />;
+    return (
+        <Image
+            animation={animation}
+            src={
+                "https://foodict.s3.ap-south-1.amazonaws.com/misc/euro-coin.png"
+            }
+            {...props}
+        />
+    );
 };
